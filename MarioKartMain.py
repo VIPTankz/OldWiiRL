e80d514ba9ab04b70b44ad59b8282ade715c8f9c
VIPTankz <41129056+VIPTankz@users.noreply.github.com>
2022-05-14 15:40:38 +0100

    really ready to start

--- MarioKartMain.py
+++ MarioKartMain.py
@@ -7,13 +7,13 @@ from MarioKartEnv import MarioKartEnv
 
 if __name__ == '__main__':
     env = MarioKartEnv()
-    save_interval = 5
+    save_interval = 100
     load_checkpoint = False
 
     agent = Agent(gamma=0.99, epsilon=1, batch_size=32, n_actions=4,
                       eps_end=0.1, input_dims=[4,32,64], lr=1e-5,
                       max_mem_size=1000000,memory = "ER",image = True,
-                      learning_starts=32,replace=1000,preprocess = True)
+                      learning_starts=20000,replace=1000,preprocess = True)
 
     if load_checkpoint:
         agent.load_models()
@@ -44,6 +44,7 @@ if __name__ == '__main__':
         arr.append([score,i,steps,round(time.time() - start,4),agent.epsilon])
         if i % save_interval == save_interval - 1:
             np.save("Results.npy",np.array(arr))
+            agent.save_models()
 
         #eps_history.append(agent.epsilon)
 
